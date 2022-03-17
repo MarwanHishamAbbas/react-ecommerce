fcba50006479acc8bc02e4941cad12050da4edbe
Marwan Hisham <marwanhiisham@outlook.com>
2022-03-17 02:26:10 +0200

    adding loading state to product page

--- src/components/shop/ProductsList.jsx
+++ src/components/shop/ProductsList.jsx
@@ -3,7 +3,7 @@ import { useSelector } from "react-redux";
 
 const ProductsList = () => {
   const products = useSelector((state) => state.products.products);
-
+  console.log(products);
   return (
     <div className=" py-16 px-0 sm:py-24">
       <div className="mt-6 grid grid-cols-1 gap-y-10 gap-x-6 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8">
